a07f1f9c56b6c7ac9743d4901ee0e71344f78cda
Amir Babic <amir.babic@rokolabs.com>
2024-01-18 13:23:49 +0100

    Update ac_webplatform_setup.sh

--- docker/ac_webplatform_with_nginx_ssl/ac_webplatform_setup.sh
+++ docker/ac_webplatform_with_nginx_ssl/ac_webplatform_setup.sh
@@ -72,22 +72,23 @@ nginx.conf
 echo "Starting installation of Docker engine!"
 sleep 2s
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sh get-docker.sh
  
 echo "Starting installation of Docker Compose plugin!"
 wget https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-linux-x86_64 -O /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 #groupadd docker
+all_users=$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd)
 getent group docker
-usermod -aG docker $USER
+usermod -aG docker $all_users
 getent group docker
 echo "break 0"
 
 newgrp docker
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-sudo systemctl start docker.service
+systemctl enable docker.service
+systemctl enable containerd.service
+systemctl start docker.service
 
 echo ""
 echo "Docker and Docker Compose successfully installed"
